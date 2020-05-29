ccb042f041497ff8f21052efca5766d5507111c8
derek-corcoran-barrios <derek.corcoran.barrios@gmailcom>
2020-05-28 20:24:20 -0400

    <Clase 9>

---
+++ CursoMulti/Scripts/Clase_9.R
@@ -0,0 +1,51 @@
+library(tidyverse)
+library(caret)
+
+Train <- read_csv("Train.csv")
+
+
+
+Graph_Slice <- function(Slices = Slices) {
+  Slice <- list()
+  for (i in 1:length(Slices$test)) {
+    Window <- Train[Slices$train[[i]], ] %>% mutate(rep = as.character(i), 
+                                                    class = "Window (Train)")
+    Horizon <- Train[Slices$test[[i]], ] %>% mutate(rep = as.character(i), 
+                                                    class = "Horizon (Test)")
+    Slice[[i]] <- bind_rows(Window, Horizon)
+  }
+  Slices <- Slice %>% purrr::reduce(bind_rows)
+  ggplot(Slices, aes(x = Fecha, y = Activos_5_por_100.000, group = Comuna)) + geom_path(aes(color = class)) + 
+    facet_wrap(~rep) + theme_bw()
+}
+
+
+Slices <- createTimeSlices(Train$Activos_5_por_100.000, horizon = 365, initialWindow = 365 * 
+                             3, fixedWindow = T, skip = 365)
+
+
+
+#####
+
+Train2 <-  Train %>% arrange(Fecha)
+
+Slices <- createTimeSlices(Train2$Activos_5_por_100.000, horizon = 365, initialWindow = 365 * 
+                             3, fixedWindow = T, skip = 365)
+
+Graph_Slice(Slices)
+
+
+####
+
+
+
+Slices <- createTimeSlices(Train2$Activos_5_por_100.000, horizon = 346*5, initialWindow = 346 * 5, fixedWindow = T, skip = 346)
+
+Graph_Slice(Slices)
+
+
+#####
+
+Slices <- createTimeSlices(Train2$Activos_5_por_100.000, horizon = 346*5, initialWindow = 346 * 5, fixedWindow = F, skip = 346)
+
+Graph_Slice(Slices)
