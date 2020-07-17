d9906a5d42586bf79a80b0fa550d0a3a5dc040b5
derek-corcoran-barrios <derek.corcoran.barrios@gmailcom>
2020-07-17 19:11:43 -0400

    Arreglar clase

--- Scripts_Espacial/Clase5.R
+++ Scripts_Espacial/Clase5.R
@@ -1,7 +1,18 @@
 library(ggforce)
 library(scales)
+library(raster)
+library(rworldxtra)
 library(sf)
 library(tidyverse)
 
 setwd("~/derek-corcoran-barrios.github.io/Scripts_Espacial")
 
+# Base de datos
+githubURL <- ("https://raw.githubusercontent.com/derek-corcoran-barrios/derek-corcoran-barrios.github.io/master/Presentaciones_Espacial/Casos_Activos_Valpo.rds")
+# La descargamos
+download.file(githubURL, "Casos_Activos_Valpo.rds")
+# La leemos
+Casos_Activos_Valpo <- readRDS("Casos_Activos_Valpo.rds")
+# Gráfico simple
+ggplot() + geom_sf(data = Casos_Activos_Valpo, aes(fill = Activos_por_100.000)) + 
+  facet_wrap(~Fecha)
