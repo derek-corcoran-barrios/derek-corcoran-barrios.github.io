463bc3228a6017c94efb447f0b199d39d5c35bb6
derek-corcoran-barrios <derek.corcoran.barrios@gmail.com>
2021-01-11 09:11:38 -0300

    Clase lista

--- CursoProgrPres/Clase11/Tests.R
+++ CursoProgrPres/Clase11/Tests.R
@@ -45,16 +45,48 @@ Map$addLayer(ndvi1, ndviPar, "NDVI verano") + Map$addLayer(ndvi2, ndviPar, "NDVI
 
 library(raster)
 library(sf)
+library(tidyverse)
+
+ee_x <- st_read(system.file("shape/nc.shp", package = "sf")) 
 
 ValpoVina <- getData("GADM", country = "CHL", level = 2) %>% 
   st_as_sf() %>% 
-  dplyr::filter(NAME_1 == "Valparaíso", NAME_2 %in% c("Valparaíso"))
+  dplyr::filter(NAME_1 == "Valparaíso", NAME_2 %in% c("Valparaíso")) %>%
+  st_transform(st_crs(ee_x)) %>% 
+  sf_as_ee()
+#  st_set_crs("+proj=utm +zone=19 +south +datum=WGS84 +units=m +no_defs")
 
 start <- ee$Date("2020-01-01")
 end <- ee$Date("2020-12-31")
 
-filter<-col$filterBounds(point)$filterDate(start,end)
-img <- filter$first()
-ndvi <- getNDVI(img)
 
-ee_ndvi_valpo <- ee_extract(x = ndvi, y = ValpoVina, sf = FALSE)
+col<-ee$ImageCollection("LANDSAT/LE07/C01/T1_8DAY_NDVI")
+filter<-col$filterBounds(point)$filterDate(start,end)$select("NDVI")
+
+ee_ndvi_valpo <- ee_extract(x = filter, y = ValpoVina, sf = FALSE)
+
+ee_ndvi_valpo2 <- ee_ndvi_valpo %>% pivot_longer(everything(), values_to = "NDVI") %>% mutate(Fecha = lubridate::ymd(str_remove_all(name, "X")))
+
+
+
+
+start <- ee$Date("2014-01-01")
+end <- ee$Date("2020-12-31")
+
+
+col<-ee$ImageCollection("LANDSAT/LE07/C01/T1_8DAY_NDVI")
+filter<-col$filterBounds(point)$filterDate(start,end)$select("NDVI")
+
+ee_ndvi_valpo <- ee_extract(x = filter, y = ValpoVina, sf = FALSE)
+
+ee_ndvi_valpo2 <- ee_ndvi_valpo %>% pivot_longer(everything(), values_to = "NDVI") %>% mutate(Fecha = lubridate::ymd(str_remove_all(name, "X")))
+ggplot(ee_ndvi_valpo2, aes(x = Fecha, y = NDVI)) + geom_path() + geom_point()
+
+
+ee_ndvi_valpo2 <- ee_ndvi_valpo2 %>% mutate(Dia = lubridate::yday(Fecha), Mes =  lubridate::month(Fecha))
+
+ggplot(ee_ndvi_valpo2, aes(x = Dia, y = NDVI)) + geom_point()
+
+ee_ndvi_valpo_summary <- ee_ndvi_valpo2 %>% group_by(Mes) %>% summarise(sd = sd(NDVI), NDVI = mean(NDVI))
+
+ggplot(ee_ndvi_valpo_summary, aes(x = Mes, y = NDVI)) + geom_ribbon(aes(ymax = NDVI + sd, ymin = NDVI -sd), alpha = 0.3, fill = "red") + geom_point() + geom_path()
