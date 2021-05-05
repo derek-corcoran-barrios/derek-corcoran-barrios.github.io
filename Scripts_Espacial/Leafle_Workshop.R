8faa4b40939cd92e7953ae8d13780f45069d6f0b
derek-corcoran-barrios <derek.corcoran.barrios@gmail.com>
2021-05-05 05:26:07 -0400

    Changed tutorial

--- Scripts_Espacial/Leafle_Workshop.R
+++ Scripts_Espacial/Leafle_Workshop.R
@@ -12,4 +12,66 @@ Bombus <- Bombus[,c(3,5:7,9)]
 
 colnames(Bombus)[5] <- "Abundance"
 
-Species <- split(Bombus, Bombus$species)
+
+
+# First quick map
+
+M <- leaflet() %>% 
+  addTiles() %>% 
+  addCircles(data = Bombus, lng = ~Longitude, ~Latitude) 
+
+M
+
+## Some control
+
+M %>% addMeasure()
+
+# color by species
+
+pal <- colorFactor(c("red", "blue"), domain = unique(Bombus$species))
+
+  
+  M <- leaflet() %>% 
+    addTiles() %>% 
+    addCircles(data = Bombus, lng = ~Longitude, ~Latitude, color = ~pal(species), fillColor = ~pal(species))  
+  
+  M %>% addLegend(pal = pal, values = unique(Bombus$species))
+
+  ## Add popup or abundace
+  
+  M <- leaflet() %>% 
+    addTiles() %>% 
+    addCircles(data = Bombus, lng = ~Longitude, ~Latitude, 
+               color = ~pal(species), fillColor = ~pal(species),
+               popup = ~Site_name, label = ~Abundance)
+  
+  M %>% addLegend(pal = pal, values = unique(Bombus$species))
+  
+  ## Or size of circles
+  
+  M <- leaflet() %>% 
+    addTiles() %>% 
+    addCircleMarkers(data = Bombus, lng = ~Longitude, ~Latitude, 
+                     color = ~pal(species), fillColor = ~pal(species),
+                     popup = ~Site_name, label = ~Abundance,
+                     radius = 10*(Bombus$Abundance/max(Bombus$Abundance)))
+  
+  M %>% addLegend(pal = pal, values = unique(Bombus$species))
+
+## To hide/ control things you need a group
+  
+  Species <- split(Bombus, Bombus$species)
+  
+  M <- leaflet() %>% 
+    addTiles() %>% 
+    addCircles(data = Species[[1]], lng = ~Longitude, ~Latitude, color = ~pal(species), fillColor = ~pal(species), group = names(Species)[1],
+               popup = ~Site_name, label = ~Abundance)  %>% 
+    addCircles(data = Species[[2]], lng = ~Longitude, ~Latitude, color = ~pal(species), fillColor = ~pal(species), group = names(Species)[2],
+               popup = ~Site_name, label = ~Abundance) %>% 
+    addLegend(pal = pal, values = unique(Bombus$species))
+  
+  
+M %>% addLayersControl(overlayGroups = names(Species))
+
+
+## Try to use one species and use the colorNumeric function to get an abundance color legend
